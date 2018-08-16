e6d13f464f03cdb0df688af5b2af0d37fc9aedc0
Pan Cheng <pchengeducation@hotmail.com>
2018-08-16 01:52:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  const rank = document.querySelectorAll('.ranked-list')
+  const rank = document.querySelectorAll('.ranked-list li')
   for (let i = 0, l = rank.length; i < l; i++){
     rank[i].innerHTML = parseInt(rank[i].innerHTML + n)
   }
+}
+
+function deepstChild(){
+  return document.querySelector('div#grand-node div div div div')
 }
